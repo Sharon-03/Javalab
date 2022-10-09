2ee3f6016f17815d0544cffbfe354e98ed62ff67
Sharon Sayaji <95227783+Sharon-03@users.noreply.github.com>
2022-10-09 13:27:33 +0530

    Update Matrixaddition.java

--- Matrixaddition.java
+++ Matrixaddition.java
@@ -4,14 +4,20 @@ public class Matrixaddition
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
-		int r,c;
+		int r,c, m, n;
 		int ar[][]= new int[20][20];
 		int br[][]= new int[20][20];
 		System.out.println("NOTE: To add two matrices they should be of same order.");
-		System.out.println("Enter the no.of rows: ");
+		System.out.println("Enter the no.of rows of matrix1: ");
 	     r = sc.nextInt();
-	     System.out.println("Enter the number of columns: ");
+	     System.out.println("Enter the number of columns of matrix2: ");
 	     c = sc.nextInt();
+             System.out.println("Enter the no.of rows of matrix2: ");
+	     m = sc.nextInt();
+             System.out.println("Enter the no.of columns of matrix2: ");
+             n = sc.nextInt() ;
+         if(r==m&&n==c) 
+          {
 	     System.out.println("Enter the elements of first matrix.");
 	     for(int i=0;i<r;i++)
 	     {
@@ -44,5 +50,10 @@ public class Matrixaddition
 	    	 }
 	    	 System.out.println("\n");
 	     }
-	}
+           }
+         else
+         {
+           System.out.println("Addition not possible.");
+         }
+      }
 }
