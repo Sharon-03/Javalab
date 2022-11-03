41d947eb2e4ae59c8b19e8c927dcdb576e7c07d3
Sharon Sayaji <95227783+Sharon-03@users.noreply.github.com>
2022-11-03 15:59:44 +0530

    Create Polymorphism.java

---
+++ Polymorphism.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+abstract class Shapes
+{
+ int a,b,c;
+ abstract void area(int a,int b,int c);
+}
+class Rectangle extends Shapes
+{
+ void area(int a,int b,int c) 
+ {
+  System.out.println("Rectangle's Area= "+(a*b));	 
+ }
+}
+class Triangle extends Shapes
+{
+ void area(int a,int b,int c) 
+ {
+  System.out.println("Triangle's Area= "+(0.5*a*b));	 
+ }
+}
+public class Polymorphism 
+{
+	public static void main(String[] args) 
+	{
+	 Rectangle rec=new Rectangle();
+	 rec.area( 5, 20,1);
+	 Triangle t=new Triangle();
+	 t.area(20, 5,1);
+	 System.out.println("FINISH");
+	}
+}
